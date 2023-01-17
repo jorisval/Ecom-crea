fe1d45e5dfb0784f984e76720a667d747323885a
Joris Awoui <jorisawoui@gmail.com>
2023-01-17 14:31:14 +0100

    set up of responsive shorp portfolio without JS

---
+++ portfolio-boutique.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=divice-width, initial-scale=1.0">
+        
+        <!-- Bootstrap icons -->
+        <link href='https://fonts.googleapis.com/css?family=Urbanist' rel='stylesheet'>
+
+        <!-- Bootstrap -->
+        <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="public/css/style.css"/>
+        
+        <title>Votre équipe !</title>
+    </head>
+    <body>
+        <?php include("header.php");?>
+        <div class="pages-title">
+            <h1>Portfolio<span class="bi bi-chevron-double-right"></span></h1>
+        </div>
+        <div class="portfolio">
+            <ul class="portfolio__menu">
+                <li class="boutiques">
+                    <a href="" class="active">Boutiques</a>
+                </li>
+                <li class="videos">
+                    <a href="" class="">Vidéos</a>
+                </li>
+            </ul>
+            <div class="portfolio__boutique-contents">
+                <img src="images/boutique-1.png">
+                <img src="images/boutique-2.png">
+                <img src="images/boutique-3.png">
+                <img src="images/boutique-4.png">
+                <img src="images/boutique-5.png">
+                <img src="images/boutique-6.png">
+            </div>
+        </div>
+        <div class="portfolio-button">
+            <button class="cta-button">Je veux les mêmes</button>
+        </div>
+        <?php include("footer.php");?>
+
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+        <script src="js/index.js"></script>
+    </body>
+</html>
