e066e39054bbe7049e88ff2d79666059d5e5b456
Joris Awoui <jorisawoui@gmail.com>
2023-03-30 15:47:31 +0100

    setting up blog page and displaying   posts using api

--- frontend/src/components/pages/Blog.jsx
+++ frontend/src/components/pages/Blog.jsx
@@ -1,75 +1,71 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { HeaderContext } from "../utils/context";
+import { useFetch } from "../utils/hooks";
 
 function Blog() {
     const { setActivePage } = useContext(HeaderContext);
     useEffect(() => {
         setActivePage("blog");
     }, [setActivePage]);
+    const { data } = useFetch('http://localhost:3000/api/post');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [perPage] = useState(8);
+    const indexOfLastPost = currentPage * perPage;
+    const indexOfFirstPost = indexOfLastPost - perPage;
+    const currentPosts = Array.isArray(data) && data?.slice(indexOfFirstPost, indexOfLastPost);
+    const pageNumber = [];
+
+    for(let i=1; i <= Math.ceil(data?.length / perPage); i++) {
+        pageNumber.push(i);
+    }
+
+    const nextButton = () => {
+        currentPage < pageNumber.length && setCurrentPage(currentPage + 1);
+    }
+    const previousButton = () => {
+        currentPage > 1 && setCurrentPage(currentPage - 1);
+    }
+
+    const formatedDate = (isoDate) => {
+        // Create a new Date object from the ISO date string
+        const date = new Date(isoDate);
+
+        // Define an array of month names
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+        // Format the date into the desired format
+        const formatDate = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+
+        return(formatDate); // "March 29, 2023"
+
+    }
+
     return(
         <div className="Blog">
             <div className="blog-title">
             <h1>Blog</h1>
         </div>
         <div className="articles">
-            <div className="article">
-                <img src="../../assets/images/article-1.png" alt=""/>
-                <h3>Organize & Manage Tasks & To-Do Lists</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live...</p>
-                <span>October 25, 2019</span>
-            </div>
-            <div className="article">
-                <img src="../../assets/images/article-2.png" alt=""/>
-                <h3>Organize & Manage Tasks & To-Do Lists</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live...</p>
-                <span>October 25, 2019</span>
-            </div>
-            <div className="article">
-                <img src="../../assets/images/article-3.png" alt=""/>
-                <h3>Organize & Manage Tasks & To-Do Lists</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live...</p>
-                <span>October 25, 2019</span>
-            </div>
-            <div className="article">
-                <img src="../../assets/images/article-4.png" alt=""/>
-                <h3>Organize & Manage Tasks & To-Do Lists</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live...</p>
-                <span>October 25, 2019</span>
-            </div>
-            <div className="article">
-                <img src="../../assets/images/article-5.png" alt=""/>
-                <h3>Organize & Manage Tasks & To-Do Lists</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live...</p>
-                <span>October 25, 2019</span>
-            </div>
-            <div className="article">
-                <img src="../../assets/images/article-6.png" alt=""/>
-                <h3>Organize & Manage Tasks & To-Do Lists</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live...</p>
-                <span>October 25, 2019</span>
-            </div>
-            <div className="article">
-                <img src="../../assets/images/article-7.png" alt=""/>
-                <h3>Organize & Manage Tasks & To-Do Lists</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live...</p>
-                <span>October 25, 2019</span>
-            </div>
-            <div className="article">
-                <img src="../../assets/images/article-8.png" alt=""/>
-                <h3>Organize & Manage Tasks & To-Do Lists</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live...</p>
-                <span>October 25, 2019</span>
-            </div>
-            <div className="article">
-                <img src="../../assets/images/article-9.png" alt=""/>
-                <h3>Organize & Manage Tasks & To-Do Lists</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live...</p>
-                <span>October 25, 2019</span>
-            </div>
+            { Array.isArray(currentPosts) && currentPosts.map((post) => {
+                return(
+                    <div className="article" key={post._id}>
+                        <img src={post.imageUrl} alt=""/>
+                        <h3>
+                            {post.title.slice(0, 40)}
+                            {post.title.length > 40 && '...'}
+                        </h3>
+                        <p>
+                            {post.content.slice(0, 150)}
+                            {post.content.length > 150 && '...'}
+                        </p>
+                        <span>{formatedDate(post.dateCreated)}</span>
+                    </div>
+                )
+            }) }
         </div>
         <div className="buttons-article">
-            <button className="previous">Previous</button>
-            <button className="next">Next</button>
+            <button className="previous" onClick={() => previousButton()}>Previous</button>
+            <button className="next" onClick={() => nextButton()}>Next</button>
         </div>
         </div>
     );
