65dd21d57affa8b68979d70f1bcd6a64c33daf75
Joris Awoui <jorisawoui@gmail.com>
2023-04-05 14:56:33 +0100

    post-user model created and post-comment model updated. functions to manage post comments and users created (createPostComment, getPostComment, deleteComment, createPostUser, getPostUsers)

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,5 +1,7 @@
 const Post = require('../models/Post');
 const fs = require('fs');
+const PostUser = require('../models/Post-user');
+const PostComment = require('../models/Post-comment');
 
 exports.createPost = (req, res, next) => {
     const postObject = req.body;
@@ -11,11 +13,13 @@ exports.createPost = (req, res, next) => {
     .then(() => res.status(201).json({ message: 'Post saved !'}))
     .catch(error => res.status(400).json({ error }));
 }
+
 exports.getOnePost = (req, res, next) => {
     Post.findOne({ _id: req.params.id })
     .then(post => res.status(200).json(post))
     .catch(error => res.status(404).json({ error }));
 }
+
 exports.modifyPost = (req, res, next) => {
     const postObject = req.file ? {
         ...JSON.parse(req.body.post),
@@ -29,6 +33,7 @@ exports.modifyPost = (req, res, next) => {
     })
     .catch(error => res.status(400).json({ error }));
 }
+
 exports.deletePost = (req, res, next) => {
     Post.findOne({ _id: req.params.id })
     .then(post => {
@@ -41,8 +46,39 @@ exports.deletePost = (req, res, next) => {
     })
     .catch(error => res.status(401).json({error }));
 }
+
 exports.getAllPosts = (req, res, next) => {
     Post.find()
     .then(posts => res.status(200).json(posts))
     .catch(error => res.status(400).json({ error }));
 }
+
+exports.createPostComment = (req, res, next) => {
+    const postCommentObject = req.body;
+    const postComment = new PostComment(postCommentObject);
+    postComment.save()
+    .then(() => res.status(201).json({ message: 'Post comment saved !'}))
+    .catch(error => res.status(400).json({ error }));
+}
+
+exports.getPostComments = (req, res, next) => {
+    PostComment.find({ post: req.params.postId })
+    .populate('author')
+    .then(comments => res.status(200).json(comments))
+    .catch(error => res.status(400).json({ error }));
+}
+
+exports.createPostUser = (req, res, next) => {
+    const postUserObject = req.body;
+    const postUser = new PostUser(postUserObject);
+    postUser.save()
+    .then(() => res.status(201).json({ message: 'Post user saved !'}))
+    .catch(error => res.status(400).json({ error }));
+}
+
+exports.getPostUsers = (req, res, next) => {
+    PostUser.find()
+    .then(users => res.status(200).json(users))
+    .catch(error => res.status(400).json({ error }));
+}
+
--- backend/models/Post-comment.js
+++ backend/models/Post-comment.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const postCommentSchema = mongoose.Schema({
     post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true},
-    author: {type: mongoose.Schema.Types.ObjectId, ref: 'ContactInfo', required: true},
+    author: {type: mongoose.Schema.Types.ObjectId, ref: 'PostUser', required: true},
     content: {type: String, required: true},
     dateCreated: {type: Date, default: Date.now}
 });
